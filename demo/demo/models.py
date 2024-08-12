e9a4bcf856947b317c0e4d159c2afd97f704a071
khushali ladher <136237640+Khushi141002@users.noreply.github.com>
2024-08-12 18:23:07 +0530

    Add files via upload

---
+++ demo/demo/models.py
@@ -0,0 +1,4 @@
+# from django.db import models
+
+# class customer(models.Model):
+#     name = models.CharField(max_length=100)
---
+++ demo/demo1/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for demo project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('notes/',views.notes,name="notes"),
+    path('delete_note/<int:pk>',views.delete_note,name="delete-note"),
+    path('notes_detail/<int:pk>',views.NotesDetailView.as_view(),name="notes-detail"),
+    
+    path('homework/',views.homework,name="homework"),
+    path('update_homework/<int:pk>',views.update_homework,name="update-homework"),
+    path('delete_homework/<int:pk>',views.delete_homework,name="delete-homework"),
+    
+    path('youtube/',views.youtube,name="youtube"),
+    
+    path('update_todo/<int:pk>',views.update_todo,name="update-todo"),
+    path('delete_todo/<int:pk>',views.delete_todo,name="delete-todo"),
+    path('todo/',views.todo,name="todo"),
+
+    path('books/',views.books,name='books'),
+    
+    path('dictionary/',views.dictionary,name='dictionary'),
+
+    path('wiki/',views.wiki,name="wiki"),
+]
